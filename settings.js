3b325189f8418cd464889b73219ed1d93390de28
barxnl-max <hibarxnl@gmail.com>
2025-12-24 17:17:24 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎Akbar',
   botName: "Lydia Bot",
   botOwner: 'Kontlo', // Your name
-  ownerNumber: '628565471603p0', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '628565471603l0', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
